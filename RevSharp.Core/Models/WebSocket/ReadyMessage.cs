7ea22f9aaa110e4549db3ecb424a4520f9a6a9c0
kate! <kate@dariox.club>
2023-04-26 23:38:56 +0800

    Add Ready Message

---
+++ RevSharp.Core/Models/WebSocket/ReadyMessage.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RevSharp.Core.Models.WebSocket;
+
+public class ReadyMessage : BaseWebSocketMessage
+{
+    [JsonPropertyName("type")]
+    public new string Type => "Ready";
+    [JsonPropertyName("users")]
+    public User[] Users { get; set; }
+    [JsonPropertyName("servers")]
+    public Server[] Servers { get; set; }
+    [JsonPropertyName("channels")]
+    public BaseChannel[] Channels { get; set; }
+    [JsonPropertyName("emojis")]
+    public Emoji[]? Emojis { get; set; }
+}
