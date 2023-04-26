b7d0f64312c5eb2a8e49cc025fdf5543438e8412
kate! <kate@dariox.club>
2023-04-26 23:39:05 +0800

    Create InterfaceHelper

---
+++ RevSharp.Core/Models/InterfaceHelpers.cs
@@ -0,0 +1,13 @@
+﻿namespace RevSharp.Core.Models;
+
+public interface IFetchable
+{
+    public Task<bool> Fetch(Client client);
+}
+public interface ISnowflake
+{
+    /// <summary>
+    /// Unique Id
+    /// </summary>
+    string Id { get; }
+}
