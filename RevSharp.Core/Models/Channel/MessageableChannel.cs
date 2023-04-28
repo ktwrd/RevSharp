4e92134add400556f3e2e79d2351dcf739601806
kate! <kate@dariox.club>
2023-04-28 20:35:43 +0800

    Add MessageReceived event handling-ish

--- RevSharp.Core/Models/Channel/MessageableChannel.cs
+++ RevSharp.Core/Models/Channel/MessageableChannel.cs
@@ -5,22 +5,33 @@ namespace RevSharp.Core.Models;
 /// <summary>
 /// A channel that can have messages in it
 /// </summary>
-public class MessageableChannel : BaseChannel, Clientable, ISnowflake
+public class MessageableChannel : BaseChannel
 {
     internal Dictionary<string, Message> MessageCache { get; private set; }
     
     #region Constructors
     public MessageableChannel()
-        : this(null, "")
+        : base(null, "")
     {}
     public MessageableChannel(string id)
-        : this(null, id)
+        : base(null, id)
     {}
-
-    internal MessageableChannel(Client client, string id)
+    internal MessageableChannel(Client? client, string id)
         : base(client, id)
     {
         MessageCache = new Dictionary<string, Message>();
+        MessageReceived += MessageReceivedHandle;
     }
     #endregion
+
+    private void MessageReceivedHandle(Message message)
+    {
+        if (MessageCache.ContainsKey(message.Id))
+        {
+            MessageCache[message.Id] = message;
+            return;
+        }
+        
+        MessageCache.Add(message.Id, message);
+    }
 }
