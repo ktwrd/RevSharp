e7e16c8a8ff6baab6291a0dbdddc454c0d8a1ae0
kate <kate@dariox.club>
2023-04-28 18:07:10 +0800

    Add MessageDelegate

--- RevSharp.Core/Helpers/Delegates.cs
+++ RevSharp.Core/Helpers/Delegates.cs
@@ -1,3 +1,6 @@
-﻿namespace RevSharp.Core.Helpers;
+﻿using RevSharp.Core.Models;
+
+namespace RevSharp.Core.Helpers;
 
 public delegate void GenericDelegate<T>(T content);
+public delegate void MessageDelegate(Message message);
