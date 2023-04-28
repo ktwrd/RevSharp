008690bcb02bd9a3b72831cfdae077fa0ee4eeb6
kate! <kate@dariox.club>
2023-04-28 19:32:15 +0800

    Remove property HasClient

--- RevSharp.Core/Models/Clientable.cs
+++ RevSharp.Core/Models/Clientable.cs
@@ -3,17 +3,14 @@ namespace RevSharp.Core.Models;
 public class Clientable
 {
     internal Client? _client { get; private set; }
-    protected bool HasClient { get; private set; }
 
     public Clientable(Client client)
     {
         _client = client;
-        HasClient = true;
     }
 
     public Clientable()
     {
         _client = null;
-        HasClient = false;
     }
 }
