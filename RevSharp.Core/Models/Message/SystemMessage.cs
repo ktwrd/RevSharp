31760433aa892b6788d059799997affc44562260
kate! <kate@dariox.club>
2023-04-28 21:37:05 +0800

    Create separate SystemMessage types

--- RevSharp.Core/Models/Message/SystemMessage.cs
+++ RevSharp.Core/Models/Message/SystemMessage.cs
@@ -1,14 +1,77 @@
-﻿namespace RevSharp.Core.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace RevSharp.Core.Models;
 
 public class SystemMessage
 {
+    [JsonPropertyName("type")]
     public string Type { get; set; }
-    public string? Content { get; set; }
-    public string? Id { get; set; }
-    public string? By { get; set; }
-    public string? Name { get; set; }
-    public string? From { get; set; }
-    public string? To { get; set; }
+}
+
+public class SystemTextMessage : SystemMessage
+{
+    [JsonPropertyName("content")]
+    public string Content { get; set; }
+}
+
+public class SystemUserAddMessage : SystemMessage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+    [JsonPropertyName("by")]
+    public string By { get; set; }
+}
+public class SystemUserRemoveMessage : SystemMessage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+}
+
+public class SystemUserJoinMessage : SystemMessage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+}
+public class SystemUserLeftMessage : SystemMessage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+}
+public class SystemUserKickedMessage : SystemMessage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+}
+public class SystemUserBannedMessage : SystemMessage
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+}
+
+public class SystemChannelRenamedMessage : SystemMessage
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("by")]
+    public string By { get; set; }
+}
+
+public class SystemChannelDescriptionRenamedMessage : SystemMessage
+{
+    [JsonPropertyName("by")]
+    public string By { get; set; }
+}
+public class SystemChannelIconChangedMessage : SystemMessage
+{
+    [JsonPropertyName("by")]
+    public string By { get; set; }
+}
+public class SystemChannelOwnershipChangedMessage : SystemMessage
+{
+    [JsonPropertyName("from")]
+    public string From { get; set; }
+    [JsonPropertyName("to")]
+    public string To { get; set; }
 }
 public enum SystemMessageType
 {
