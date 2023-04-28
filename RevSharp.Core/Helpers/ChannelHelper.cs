01f2cb196d933ba7aa94e8a0ec0691c37d855339
kate! <kate@dariox.club>
2023-04-28 21:53:20 +0800

    Create method to parse json into extended channel type

---
+++ RevSharp.Core/Helpers/ChannelHelper.cs
@@ -0,0 +1,32 @@
+﻿using RevSharp.Core.Models;
+using Newtonsoft.Json.Linq;
+using STJson = System.Text.Json.JsonSerializer;
+
+namespace RevSharp.Core.Helpers;
+
+public static class ChannelHelper
+{
+    private static readonly Dictionary<string, Type> ChannelTypeMap = new Dictionary<string, Type>()
+    {
+        {"SavedMessages", typeof(SavedMessagesChannel)},
+        {"DirectMessage", typeof(DirectMessageChannel)},
+        {"Group", typeof(GroupChannel)},
+        {"TextChannel", typeof(TextChannel)},
+        {"VoiceChannel", typeof(VoiceChannel)}
+    };
+
+    public static BaseChannel? ParseChannel(string json)
+    {
+        var channelObject = STJson.Deserialize<BaseChannel>(json, Client.SerializerOptions);
+        if (channelObject == null)
+            return null;
+
+        var channelTypeName = channelObject.ChannelType;
+        if (ChannelTypeMap.TryGetValue(channelTypeName, out var value))
+        {
+            return (BaseChannel)JObject.Parse(json).ToObject(value)!;
+        }
+
+        return null;
+    }
+}
