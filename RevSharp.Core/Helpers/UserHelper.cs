d9f597fb4b6005c06e54a28714f118c681d4612b
kate! <kate@dariox.club>
2023-04-28 20:35:56 +0800

    Create UserHelper

---
+++ RevSharp.Core/Helpers/UserHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace RevSharp.Core.Helpers;
+
+internal static class UserHelper
+{
+    internal static async Task<bool> ChangeUsername(Client client, string username, string password)
+    {
+        var response = await client.PatchAsync("/users/@me/username", new Dictionary<string, object>()
+        {
+            { "username", username },
+            { "password", password }
+        });
+        if (response.StatusCode != HttpStatusCode.OK)
+            return false;
+
+        if (client.CurrentUser != null)
+            return await client.CurrentUser.Fetch(client);
+        return true;
+    }
+}
