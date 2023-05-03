6dd5456c5f73cea486a9876e115a1ca8dbfc8ad0
kate! <kate@dariox.club>
2023-05-03 20:45:01 +0800

    Add MessageReactedEvent

---
+++ RevSharp.Core/Models/WebSocket/MessageReactedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RevSharp.Core.Models.WebSocket;
+
+public class MessageReactedEvent : BaseTypedResponse
+{
+    [JsonPropertyName("id")]
+    public string MessageId { get; set; }
+    [JsonPropertyName("channel_id")]
+    public string ChannelId { get; set; }
+    [JsonPropertyName("user_id")]
+    public string UserId { get; set; }
+    [JsonPropertyName("emoji_id")]
+    public string Emoji { get; set; }
+}
