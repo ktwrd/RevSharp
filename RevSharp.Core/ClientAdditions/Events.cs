41fba99c9f9844ce19ff19452fc298de05e8fe49
kate! <kate@dariox.club>
2023-05-03 20:30:45 +0800

    Add event MessageDeleted

--- RevSharp.Core/ClientAdditions/Events.cs
+++ RevSharp.Core/ClientAdditions/Events.cs
@@ -9,4 +9,13 @@ public partial class Client
     public event VoidDelegate Ready;
     public event VoidDelegate ClientAuthenticated;
     public event StringDelegate ErrorReceived;
+    public event MessageDeleteDelegate MessageDeleted;
+
+    internal void OnMessageDeleted(string messageId, string channelId)
+    {
+        if (MessageDeleted != null)
+        {
+            MessageDeleted?.Invoke(messageId, channelId);
+        }
+    }
 }
