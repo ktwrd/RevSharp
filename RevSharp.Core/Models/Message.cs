256bf307a7c1f05a9fa72b733603133f3dd5d23d
kate <kate@dariox.club>
2023-05-05 20:02:42 +0800

    Create SendableEmbed

--- RevSharp.Core/Models/Message.cs
+++ RevSharp.Core/Models/Message.cs
@@ -135,3 +135,25 @@ public partial class Message : Clientable, ISnowflake
         ReactRemove?.Invoke(userId, react, Id);
     }
 }
+public class SendableMessage
+{
+    [JsonPropertyName("nonce")]
+    public string? Nonce { get; set; }
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
+    [JsonPropertyName("replies")]
+    public Reply[]? Replies { get; set; }
+    [JsonPropertyName("embeds")]
+    public SendableEmbed[]? Embeds { get; set; }
+    [JsonPropertyName("masquerade")]
+    public Masquerade? Masquerade { get; set; }
+    [JsonPropertyName("attachments")]
+    public string[] Attachments { get; set; }
+
+    public SendableMessage()
+    {
+        Nonce = GeneralHelper.GenerateUID();
+        Replies = Array.Empty<Reply>();
+        Embeds = Array.Empty<SendableEmbed>();
+    }
+}
