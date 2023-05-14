2750b176745d7f11803a5b49237b7bcb3cfcbb16
kate <kate@dariox.club>
2023-05-15 00:32:52 +0800

    Move badges to separate file

--- RevSharp.Core/Models/User/IUser.cs
+++ RevSharp.Core/Models/User/IUser.cs
@@ -55,53 +55,7 @@ public interface IUser : ISnowflake, IFetchable
     public bool IsOnline { get; set; }
 }
 
-public enum Badges
-{
-    /// <summary>
-    /// Revolt Developer
-    /// </summary>
-    Developer = 1,
-    /// <summary>
-    /// Helped translate revolt
-    /// </summary>
-    Translator = 2,
-    /// <summary>
-    /// Monetarily supported Revolt
-    /// </summary>
-    Supporter = 4,
-    /// <summary>
-    /// Responsibly disclosed a security issue
-    /// </summary>
-    ResponsibleDisclosure = 8,
-    /// <summary>
-    /// Revolt Founder
-    /// </summary>
-    Founder = 16,
-    /// <summary>
-    /// Platform moderator
-    /// </summary>
-    PlatformModeration = 32,
-    /// <summary>
-    /// Active monetary supporter
-    /// </summary>
-    ActiveSupporter = 64,
-    /// <summary>
-    /// 🦊🦝
-    /// </summary>
-    Paw = 128,
-    /// <summary>
-    /// Joined as one of the first 1000 users in 2021
-    /// </summary>
-    EarlyAdopter = 256,
-    /// <summary>
-    /// Amogus
-    /// </summary>
-    ReservedRelevantJokeBadge1 = 512,
-    /// <summary>
-    /// Low resolution troll face
-    /// </summary>
-    ReservedRelevantJokeBadge2  = 1024
-}
+
 public interface IUserStatus
 {
     /// <summary>
