66721f605426d18daac2726ae7d89e5906cdfeaf
kate <kate@dariox.club>
2023-05-14 14:59:10 +0800

    Create CommandHelper.cs

---
+++ RevSharp.ReBot/Helpers/CommandHelper.cs
@@ -0,0 +1,32 @@
+namespace RevSharp.ReBot.Helpers;
+
+public class CommandInfo
+{
+    public string Command { get; set; }
+    public List<string> Arguments { get; set; }
+    public string Content { get; set; }
+}
+public static class CommandHelper
+{
+    public static CommandInfo? FetchInfo(string prefix, string content)
+    {
+        if (!content.StartsWith(prefix))
+            return null;
+        
+        var substr = content.Substring(prefix.Length);
+        var args = substr.Split(" ").ToList();
+        var cmd = args[0];
+        // if no command and it's "r." then we ignore
+        if (args.Count < 1)
+            return null;
+        
+        // remove cmd
+        args.RemoveAt(0);
+        return new CommandInfo()
+        {
+            Command = cmd,
+            Arguments = args,
+            Content = content
+        };
+    }
+}
