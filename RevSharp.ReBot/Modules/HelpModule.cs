d55efdcb418ab090968c61e8861c112848643a7f
kate <kate@dariox.club>
2023-05-14 18:25:26 +0800

    Refactor help module

--- RevSharp.ReBot/Modules/HelpModule.cs
+++ RevSharp.ReBot/Modules/HelpModule.cs
@@ -7,7 +7,43 @@ namespace RevSharp.ReBot.Modules;
 [RevSharpModule]
 public class HelpModule : BaseModule
 {
-    public Dictionary<string, string> HelpDict => new();
+    public override bool HasHelpContent => false;
+    public override string? InternalName => "help";
+    public Dictionary<string, string> HelpDict = new();
+
+    public Dictionary<string, Dictionary<string, string>> HelpCategoryDictionary = new()
+    {
+        {"other", new Dictionary<string, string>()}
+    };
+    public override Task Initialize(ReflectionInclude reflection)
+    {
+        foreach (var item in reflection.FetchModules())
+        {
+            if (!item.HasHelpContent)
+                continue;
+            
+            var key = item.InternalName;
+            if (key == null)
+                continue;
+            
+            var content = item.HelpContent();
+            if (content == null)
+                continue;
+
+            HelpDict.TryAdd(key, content);
+            string category = (item.HelpCategory ?? "other")
+                .Trim()
+                .ToLower();
+            
+            Console.WriteLine($"contains: {category} {HelpCategoryDictionary.ContainsKey(category)}");
+            if (!HelpCategoryDictionary.ContainsKey(category))
+                HelpCategoryDictionary.Add(category, new Dictionary<string, string>());
+            Console.WriteLine($"contains: {category} {HelpCategoryDictionary.ContainsKey(category)}");
+            if (!HelpCategoryDictionary[category].ContainsKey(key))
+                HelpCategoryDictionary[category].Add(key, content);
+        }        
+        return Task.CompletedTask;
+    }
     public override async Task MessageReceived(Message message)
     {
         var info = CommandHelper.FetchInfo("r.", message.Content);
@@ -23,19 +59,8 @@ public class HelpModule : BaseModule
         // No arguments given. Print out summary
         if (info.Arguments.Count < 1)
         {
-            var text = new List<string>()
-            {
-                "To see the usage of a command. Run `r.help <module>` and replace `<module>` with the name of the module you'd like to use",
-                "",
-                "## Modules",
-            };
-            foreach (var pair in HelpDict)
-            {
-                text.Add($"- `{pair.Key}`");
-            }
-
             embed.Colour = "orange";
-            embed.Description = string.Join("\n", text);
+            embed.Description = HelpContent();
             await message.Reply(embed);
             return;
         }
@@ -53,5 +78,42 @@ public class HelpModule : BaseModule
             embed.Colour = "red";
             await message.Reply(embed);
         }
+    }
+
+    public override string? HelpContent()
+    {
+        var text = new List<string>()
+        {
+            "To see the usage of a command. Run `r.help <module>` and replace `<module>` with the name of the module you'd like to use.",
+            "For example, you can do `r.help dice` to see the usage for the dice command.",
+            "",
+        };
+        foreach (var categoryPair in HelpCategoryDictionary)
+        {
+            // ignore empty categories
+            if (categoryPair.Value.Count < 1)
+                continue;
+            var key = categoryPair.Key;
+            var formattedKey = "";
+            char? previousIndex = null;
+            for (int i = 0; i < key.Length; i++)
+            {
+                char target = key[i];
+                if (target == '_')
+                    target = ' ';
+
+                if (previousIndex == null || previousIndex == ' ')
+                    target = char.ToUpper(target);
+
+                formattedKey += target;
+                previousIndex = target;
+            }
+            text.Add($"### {formattedKey}");
+            var inner = categoryPair.Value
+                .Select(v => $"- `{v.Key}`");
+            text.AddRange(inner);
+        }
+
+        return string.Join("\n", text);
     }
 }
