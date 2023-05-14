29996b7ba812fdc9dd650529c307d0004f5e3e84
kate <kate@dariox.club>
2023-05-14 16:10:01 +0800

    Add comments

--- RevSharp.ReBot/Modules/HelpModule.cs
+++ RevSharp.ReBot/Modules/HelpModule.cs
@@ -7,7 +7,7 @@ namespace RevSharp.ReBot.Modules;
 [RevSharpModule]
 public class HelpModule : BaseModule
 {
-    public Dictionary<string, string> HelpDict = new Dictionary<string, string>();
+    public Dictionary<string, string> HelpDict => new();
     public override async Task MessageReceived(Message message)
     {
         var info = CommandHelper.FetchInfo("r.", message.Content);
@@ -19,16 +19,20 @@ public class HelpModule : BaseModule
             Title = "Help",
             Colour = "white"
         };
+        
+        // No arguments given. Print out summary
         if (info.Arguments.Count < 1)
         {
             var text = new List<string>()
             {
-                "To see the usage of a command. Do `r.help <module>`",
+                "To see the usage of a command. Run `r.help <module>` and replace `<module>` with the name of the module you'd like to use",
                 "",
                 "## Modules",
             };
             foreach (var pair in HelpDict)
+            {
                 text.Add($"- `{pair.Key}`");
+            }
 
             embed.Colour = "orange";
             embed.Description = string.Join("\n", text);
@@ -36,16 +40,18 @@ public class HelpModule : BaseModule
             return;
         }
 
+        // 0th argument given is a valid module. print out summary
         if (HelpDict.TryGetValue(info.Arguments[0], out var item))
         {
             embed.Description = item;
             await message.Reply("", embeds: new[] { embed });
-            return;
         }
-
-        embed.Description = $"Module `{info.Arguments[0]}` not found";
-        embed.Colour = "red";
-        await message.Reply("", embeds: new[] { embed });
-
+        // 0th argument given isn't a valid module. print error
+        else
+        {
+            embed.Description = $"Module `{info.Arguments[0]}` not found";
+            embed.Colour = "red";
+            await message.Reply("", embeds: new[] { embed });
+        }
     }
 }
