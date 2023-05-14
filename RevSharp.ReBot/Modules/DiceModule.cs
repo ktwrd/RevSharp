b289f25f3ff0e57fd2882abb80e3f42f10b9b4a0
kate <kate@dariox.club>
2023-05-14 15:15:18 +0800

    Add dice command

---
+++ RevSharp.ReBot/Modules/DiceModule.cs
@@ -0,0 +1,81 @@
+using System.Diagnostics;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using RevSharp.Core.Models;
+using RevSharp.ReBot.Helpers;
+using RevSharp.ReBot.Reflection;
+
+namespace RevSharp.ReBot.Modules;
+
+[RevSharpModule]
+public class DiceModule : BaseModule
+{
+    public override async Task MessageReceived(Message message)
+    {
+        var info = CommandHelper.FetchInfo("r.", message.Content);
+        if (info == null)
+            return;
+        if (info.Command != "dice")
+            return;
+
+        var numberRegex = new Regex(@"^[0-9]+$");
+        int min = 0;
+        int max = 0;
+        var embed = new SendableEmbed()
+        {
+            Title = "Dice"
+        };
+        if (info.Arguments.Count < 1)
+        {
+            embed.Title += " - Usage";
+            embed.Description = string.Join("\n", new string[]
+            {
+                "```",
+                "r.dice <min> <max>",
+                "r.dice <max>",
+                "",
+                "max: Maximum number for dice roll",
+                "min: Minimum number for dice roll (default: 0)",
+                "```"
+            });
+            embed.Colour = "red";
+            await message.Reply("", embeds: new[] { embed });
+            return;
+        }
+
+        
+        if (!numberRegex.IsMatch(info.Arguments[0]))
+        {
+            embed.Title += " - Error";
+            embed.Description = $"Invalid Argument `{info.Arguments[0]}`";
+            embed.Colour = "red";
+            await message.Reply("", embeds: new[] { embed });
+            return;
+        }
+
+        if (info.Arguments.Count > 1)
+        {
+            if (!numberRegex.IsMatch(info.Arguments[1]))
+            {
+                embed.Title += " - Error";
+                embed.Description = $"Invalid Argument `{info.Arguments[1]}`";
+                embed.Colour = "red";
+                await message.Reply("", embeds: new[] { embed });
+                return;
+            }
+        }
+        if (info.Arguments.Count < 2)
+        {
+            max = int.Parse(info.Arguments[0]);
+        }
+        else if (info.Arguments.Count < 3)
+        {
+            min = int.Parse(info.Arguments[0]);
+            max = int.Parse(info.Arguments[1]);
+        }
+
+        int result = Program.Random.Next(min, max);
+        embed.Description = $"## {result}";
+        await message.Reply("", embeds: new[] { embed });
+    }
+}
