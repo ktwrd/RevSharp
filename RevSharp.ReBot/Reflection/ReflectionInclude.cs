d04acd49fde4ffc140b46afce0d79b0efc8de9ba
kate <kate@dariox.club>
2023-05-14 15:55:56 +0800

    Add method FetchModule

--- RevSharp.ReBot/Reflection/ReflectionInclude.cs
+++ RevSharp.ReBot/Reflection/ReflectionInclude.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.Design.Serialization;
 using System.Reflection;
 using RevSharp.Core;
 
@@ -8,9 +9,11 @@ public class ReflectionInclude
     public ReflectionInclude(Client client)
     {
         _client = client;
+        Modules = new List<BaseModule>();
     }
     private readonly Client _client;
 
+    private List<BaseModule> Modules { get; set; }
     public async Task Search(Assembly assembly)
     {
         Console.WriteLine($"[ReflectionInclude] Searching");
@@ -21,9 +24,27 @@ public class ReflectionInclude
         foreach (var i in typesWithAttr)
         {
             var instance = (BaseModule)Activator.CreateInstance(i);
-            await InitializeEvents(instance, i);
+            instance.Reflection = this;
+            Modules.Add(instance);
+            // await InitializeEvents(instance, i);
         }
+
+        foreach (var i in Modules)
+            await InitializeEvents(i, i.GetType());
         Console.WriteLine($"[ReflectionInclude] Init {typesWithAttr.Count()} modules");
+    }
+
+    public T? FetchModule<T>() where T : BaseModule
+    {
+        foreach (var item in Modules)
+        {
+            var type = item.GetType();
+            var genericType = typeof(T);
+            if (type.FullName == genericType.FullName)
+                return (T)item;
+        }
+
+        return null;
     }
     private async Task InitializeEvents<T>(T item, Type type) where T : BaseModule
     {
@@ -33,7 +54,7 @@ public class ReflectionInclude
             await item.MessageReceived(m);
         };
         
-        await item.Initialize();
+        await item.Initialize(this);
         Console.WriteLine($"[ReflectionInclude] Init {type.Name}");
     }
 }
