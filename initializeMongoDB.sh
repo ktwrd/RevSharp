729c3e9b6ba5e84e107348faa4373391829cf144
kate <kate@dariox.club>
2023-05-15 16:08:06 +0800

    Create initializeMongoDB.sh

---
+++ initializeMongoDB.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run \
+	-v mongoData:/data/db \
+	-p 27021:27017 \
+	--name mongodb-skidbot-revolt \
+	-d \
+	-e MONGO_INITDB_ROOT_USERNAME=user \
+	-e MONGO_INITDB_ROOT_PASSWORD=password mongo 
