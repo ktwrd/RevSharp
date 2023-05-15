c0849126ce112ba52d4d45c53c251a453ba6c2a7
kate <kate@dariox.club>
2023-05-15 16:07:51 +0800

    Move enum LogFlag to separate file

--- RevSharp.Core/FeatureFlags.cs
+++ RevSharp.Core/FeatureFlags.cs
@@ -47,14 +47,3 @@ internal static class FeatureFlags
         }
     }
 }
-
-public enum LogFlag : int
-{
-    Critical = 0,
-    Error = 10,
-    Warning = 20,
-    Information = 30,
-    Note = 35,
-    Verbose = 40,
-    Debug = 50
-}
---
+++ RevSharp.Core/LogFlag.cs
@@ -0,0 +1,12 @@
+namespace RevSharp.Core;
+
+public enum LogFlag : int
+{
+    Critical = 0,
+    Error = 10,
+    Warning = 20,
+    Information = 30,
+    Note = 35,
+    Verbose = 40,
+    Debug = 50
+}
