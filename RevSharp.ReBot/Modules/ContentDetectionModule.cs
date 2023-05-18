8c333a1886eb420f59da4c749f95c9a2d78add4a
kate <kate@dariox.club>
2023-05-18 23:23:45 +0800

    add logic for more commands (disable/enable/reject/accept is left)

--- RevSharp.ReBot/Modules/ContentDetectionModule.cs
+++ RevSharp.ReBot/Modules/ContentDetectionModule.cs
@@ -46,6 +46,130 @@ public class ContentDetectionModule : BaseModule
         });
     }
 
+    private async Task Command_Status(CommandInfo info, Message message)
+    {
+        var embed = new SendableEmbed()
+        {
+            Title = "Content Detection - Status"
+        };
+        
+        var server = await message.FetchServer();
+        var controller = Reflection.FetchModule<ContentDetectionServerConfigController>();
+        var data = await controller.Get(server.Id) ??
+                   new AnalysisServerConfig()
+                   {
+                       ServerId = server.Id
+                   };
+
+        embed.Description = string.Join(
+            "\n", new string[]
+            {
+                "```", $"   Can Enable: {data.AllowAnalysis}", $"   Enabled   : {data.Enabled}",
+                $"   Banned    : {data.IsBanned}",
+                data is
+                {
+                    HasRequested: true,
+                    AllowAnalysis: false,
+                    IsBanned: false
+                }
+                    ? " Awaiting Manual Approval"
+                    : "",
+                "```"
+            });
+        await message.Reply(embed);
+    }
+    private async Task Command_Request(CommandInfo info, Message message)
+    {
+        var embed = new SendableEmbed()
+        {
+            Title = "Content Detection - Request Access"
+        };
+        
+        
+        var server = await message.FetchServer();
+        var member = await server.GetMember(message.AuthorId);
+        if (!await member.HasPermission(PermissionFlag.ManageServer))
+        {
+            embed.Description = $"You do not have the required permission, `ManageServer`";
+            embed.Colour = "red";
+            await message.Reply(embed);
+            return;
+        }
+        
+        var controller = Reflection.FetchModule<ContentDetectionServerConfigController>();
+        var data = await controller.Get(server.Id) ??
+                   new AnalysisServerConfig()
+                   {
+                       ServerId = server.Id
+                   };
+        if (data.IsBanned)
+        {
+            embed.Description = $"Server is banned\nReason: `{data.BanReason}`";
+            embed.Colour = "red";
+        }
+        else if (data.HasRequested)
+        {
+            embed.Description = data.AllowAnalysis switch
+            {
+                true => "You already have access",
+                false => "Your server is still pending approval. Please wait",
+            };
+            embed.Colour = "yellow";
+        }
+        else if (data.AllowAnalysis)
+        {
+            embed.Description = "You already have access";
+            embed.Colour = "yellow";
+        }
+        else
+        {
+            bool success = await RequestAccess(server.Id);
+            if (success)
+            {
+                embed.Description = "Request sent. Please wait for a moderator to approve your server";
+                embed.Colour = "green";
+            }
+            else
+            {
+                embed.Description = "Failed to request access";
+                embed.Colour = "red";
+            }
+        }
+
+        await message.Reply(embed);
+    }
+    private async Task<bool> RequestAccess(string serverId)
+    {
+        var server = await Client.GetServer(serverId);
+        var controller = Reflection.FetchModule<ContentDetectionServerConfigController>();
+        var data = await controller.Get(server.Id) ??
+                   new AnalysisServerConfig()
+                   {
+                       ServerId = server.Id
+                   };
+        if (data.IsBanned)
+            return false;
+        if (data.AllowAnalysis)
+            return false;
+
+        data.HasRequested = true;
+        data.AllowAnalysis = false;
+
+        var notificationChannel = await Client.GetChannel("01H0QMAXMHXT5YF5RD61ZGK1SZ") as TextChannel;
+        await notificationChannel.SendMessage(
+            new SendableEmbed()
+            {
+                Description = string.Join(
+                    "\n", new string[]
+                    {
+                        $"```", $"ServerId: {server.Id}", $"Name: {server.Name}", $"Members: {server.Members.Count}",
+                        $"```"
+                    })
+            });  
+        
+        return true;
+    }
+    
     private async Task Command_ThresholdGet(CommandInfo info, Message message)
     {
         var embed = new SendableEmbed()
@@ -81,7 +205,6 @@ public class ContentDetectionModule : BaseModule
         
         await controller.Set(data);
     }
-    
     private async Task Command_ThresholdSet(CommandInfo info, Message message)
     {
         var embed = new SendableEmbed()
@@ -253,7 +376,6 @@ public class ContentDetectionModule : BaseModule
         embed.Colour = "greed";
         await message.Reply(embed);
     }
-
     private async Task Command_SetLogChannel(CommandInfo info, Message message)
     {
         var embed = new SendableEmbed()
@@ -321,6 +443,10 @@ public class ContentDetectionModule : BaseModule
             await Command_ThresholdSet(info, message);
         else if (action == "setlogchannel")
             await Command_SetLogChannel(info, message);
+        else if (action == "status")
+            await Command_Status(info, message);
+        else if (action == "request")
+            await Command_Request(info, message);
         else
         {
             embed.Description = $"Action `{action}` not implemented";
