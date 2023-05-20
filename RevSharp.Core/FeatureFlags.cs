72d477adccd89838808a9b63f3cdf1afaee1c54c
kate <kate@dariox.club>
2023-05-20 15:31:29 +0800

    Add comments to regex

--- RevSharp.Core/FeatureFlags.cs
+++ RevSharp.Core/FeatureFlags.cs
@@ -8,6 +8,13 @@ namespace RevSharp.Core;
 /// </summary>
 internal static class FeatureFlags
 {
+    /// <summary>
+    /// Parses an environment variable as a boolean. When trimmed&lowercased to `true` it will return true, but anything else will return `false`.
+    /// When the environment variable isn't found, it wil default to <see cref="defaultValue"/>
+    /// </summary>
+    /// <param name="environmentKey"></param>
+    /// <param name="defaultValue">Used when environment variable is not set.</param>
+    /// <returns>`true` when envar is true, `false` when not true, <see cref="defaultValue"/> when not found.</returns>
     private static bool ParseBool(string environmentKey, bool defaultValue)
     {
         var item = Environment.GetEnvironmentVariable(environmentKey)
@@ -16,11 +23,23 @@ internal static class FeatureFlags
         return item == "true";
     }
 
+    /// <summary>
+    /// Just <see cref="Environment.GetEnvironmentVariable(string variable)"/> but when null it's <see cref="defaultValue"/>
+    /// </summary>
     private static string ParseString(string environmentKey, string defaultValue)
     {
         return Environment.GetEnvironmentVariable(environmentKey) ?? defaultValue;
     }
 
+    /// <summary>
+    /// Parse an environment variable as <see cref="Int32"/>.
+    /// 
+    /// - Fetch Environment variable (when null, set to <see cref="defaultValue"/> as string)
+    /// - Do regex match ^([0-9]+)$
+    /// - When success, parse item as integer then return
+    /// - When fail, return default value
+    /// </summary>
+    /// <returns></returns>
     private static int ParseInt(string envKey, int defaultValue)
     {
         var item = Environment.GetEnvironmentVariable(envKey) ?? defaultValue.ToString();
@@ -36,9 +55,31 @@ internal static class FeatureFlags
         return defaultValue;
     }
 
+    /// <summary>
+    /// Key: REVSHARP_DEBUG_WSLOG
+    /// Default: false
+    /// 
+    /// Print to console all websocket messages sent/received.
+    /// </summary>
     internal static bool WebsocketDebugLogging => ParseBool("REVSHARP_DEBUG_WSLOG", false);
+    
+    /// <summary>
+    /// Key: REVSHARP_LOG_COLOR
+    /// Default: true
+    /// 
+    /// Change console text/background color on logging.
+    /// </summary>
     internal static bool EnableLogColor => ParseBool("REVSHARP_LOG_COLOR", true);
 
+    /// <summary>
+    /// Key: REVSHARP_LOGFLAG
+    /// Default: 30
+    /// 
+    /// What log level to use. Any log level with a value equal or lower than this will be printed.
+    ///
+    /// Note:
+    /// The value will be parsed as an integer then type casted to <see cref="LogFlag"/>.
+    /// </summary>
     internal static LogFlag LogFlags
     {
         get
--- RevSharp.ReBot/FeatureFlags.cs
+++ RevSharp.ReBot/FeatureFlags.cs
@@ -4,19 +4,38 @@ namespace RevSharp.ReBot;
 
 internal static class FeatureFlags
 {
+    /// <summary>
+    /// Parses an environment variable as a boolean. When trimmed&lowercased to `true` it will return true, but anything else will return `false`.
+    /// When the environment variable isn't found, it wil default to <see cref="defaultValue"/>
+    /// </summary>
+    /// <param name="environmentKey"></param>
+    /// <param name="defaultValue">Used when environment variable is not set.</param>
+    /// <returns>`true` when envar is true, `false` when not true, <see cref="defaultValue"/> when not found.</returns>
     private static bool ParseBool(string environmentKey, bool defaultValue)
     {
         var item = Environment.GetEnvironmentVariable(environmentKey)
-                   ?? $"{defaultValue}";
+            ?? $"{defaultValue}";
         item = item.ToLower().Trim();
         return item == "true";
     }
 
+    /// <summary>
+    /// Just <see cref="Environment.GetEnvironmentVariable(string variable)"/> but when null it's <see cref="defaultValue"/>
+    /// </summary>
     private static string ParseString(string environmentKey, string defaultValue)
     {
         return Environment.GetEnvironmentVariable(environmentKey) ?? defaultValue;
     }
 
+    /// <summary>
+    /// Parse an environment variable as <see cref="Int32"/>.
+    /// 
+    /// - Fetch Environment variable (when null, set to <see cref="defaultValue"/> as string)
+    /// - Do regex match ^([0-9]+)$
+    /// - When success, parse item as integer then return
+    /// - When fail, return default value
+    /// </summary>
+    /// <returns></returns>
     private static int ParseInt(string envKey, int defaultValue)
     {
         var item = Environment.GetEnvironmentVariable(envKey) ?? defaultValue.ToString();
@@ -32,14 +51,32 @@ internal static class FeatureFlags
         return defaultValue;
     }
 
+    /// <summary>
+    /// Key: REVSHARP_LOG_COLOR
+    /// Default: true
+    /// 
+    /// Change console text/background color on logging.
+    /// </summary>
     internal static bool EnableLogColor => ParseBool("REVSHARP_LOG_COLOR", true);
 
+    /// <summary>
+    /// Key: REBOT_DATA_DIR
+    /// Default: ./data/
+    /// 
+    /// Directory where all file-based data is stored. Used for caching and whatnot.
+    /// </summary>
     internal static string DataDirectory =>
         ParseString(
             "REBOT_DIR_DATA", Path.Join(
                 Directory.GetCurrentDirectory(),
                 "data"));
 
+    /// <summary>
+    /// Key: REBOT_DIR_IACD
+    /// Default: ./data/iacd/
+    /// 
+    /// Directory for cached data.
+    /// </summary>
     internal static string ImageAnnotatorCacheDirectory =>
         ParseString(
             "REBOT_DIR_IACD", Path.Join(
