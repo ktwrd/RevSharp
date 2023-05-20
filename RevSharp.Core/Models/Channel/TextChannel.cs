1b8ecf97770640510d3501e2ba7972fca89fd3a8
kate <kate@dariox.club>
2023-05-20 17:40:56 +0800

    Add RolePermission methods from VoiceChannel

--- RevSharp.Core/Models/Channel/TextChannel.cs
+++ RevSharp.Core/Models/Channel/TextChannel.cs
@@ -64,4 +64,33 @@ public class TextChannel : BaseChannel, IFetchable
 
         return true;
     }
+
+    public async Task<bool> SetRolePermission(Client client, string roleId, long allow, long deny)
+    {
+        var pushData = new
+        {
+            permission = new
+            {
+                allow = allow,
+                deny = deny
+            }
+        };
+        var response = await client.PutAsync(
+            $"/channels/{Id}/permissions/{roleId}",
+            JsonContent.Create(pushData, options: Client.SerializerOptions));
+        if (response.StatusCode != HttpStatusCode.OK)
+            return false;
+
+        await Fetch(client);
+        return true;
+    }
+
+    public Task<bool> SetRolePermission(string roleId, long allow, long deny) =>
+        SetRolePermission(Client, roleId, allow, deny);
+
+    public Task<bool> SetDefaultPermission(Client client, long allow, long deny) =>
+        SetRolePermission(client, "default", allow, deny);
+
+    public Task<bool> SetDefaultPermission(long allow, long deny) =>
+        SetDefaultPermission(Client, allow, deny);
 }
