6d2283ea6e624c6aa5219328d2eecb7ac74a42b6
kate <kate@dariox.club>
2023-05-28 16:45:53 +0800

    Add authentik administration

--- RevSharp.ReBot/Config/ConfigData.cs
+++ RevSharp.ReBot/Config/ConfigData.cs
@@ -8,6 +8,9 @@ public class ConfigData
     public string MongoConnectionUrl { get; set; }
     public string MongoDatabaseName { get; set; }
     public GCSConfig GoogleCloud { get; set; }
+    public string[] OwnerUserIds { get; set; }
+    public string AuthentikUrl { get; set; }
+    public string AuthentikToken { get; set; }
 
     public ConfigData()
     {
@@ -17,5 +20,8 @@ public class ConfigData
         MongoConnectionUrl = "mongodb://user:password@localhost:27021";
         MongoDatabaseName = "skidbot_revolt";
         GoogleCloud = new GCSConfig();
+        OwnerUserIds = Array.Empty<string>();
+        AuthentikUrl = "";
+        AuthentikToken = "";
     }
 }
