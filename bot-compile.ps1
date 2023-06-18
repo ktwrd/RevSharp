96f90b79b6a470af71b26cd500b539b7290dfc8a
kate <kate@dariox.club>
2023-06-18 19:09:58 +0800

    Create compile script for publishing skidbot

---
+++ bot-compile.ps1
@@ -0,0 +1,19 @@
+param(
+	[string]
+	$TargetName="ktwrd/skidbot-revolt",
+	[string]
+	$TargetTag="latest",
+	[bool]
+	$IncludeTimeTag=1
+)
+$targetTag="$($TargetName):$($TargetTag)"
+docker build -t skidbot-revolt:latest .
+if ($IncludeTimeTag -eq $True)
+{
+	$currentTimeTag=@(Get-Date -UFormat %s -Millisecond 0)
+	$timeTag="$($TargetName):$($currentTimeTag)"
+	docker tag skidbot-revolt:latest $timeTag
+	docker push $timeTag
+}
+docker tag skidbot-revolt:latest $targetTag
+docker push $targetTag
