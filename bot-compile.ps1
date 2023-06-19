4ea23e677f264d7b0972e7c140cfd83b307e1cc5
kate <kate@dariox.club>
2023-06-20 07:18:46 +0800

    Publish latest tag before timestamp tag

--- bot-compile.ps1
+++ bot-compile.ps1
@@ -8,6 +8,10 @@ param(
 )
 $targetTag="$($TargetName):$($TargetTag)"
 docker build -t skidbot-revolt:latest .
+
+docker tag skidbot-revolt:latest $targetTag
+docker push $targetTag
+
 if ($IncludeTimeTag -eq $True)
 {
 	$currentTimeTag=@(Get-Date -UFormat %s -Millisecond 0)
@@ -15,5 +19,3 @@ if ($IncludeTimeTag -eq $True)
 	docker tag skidbot-revolt:latest $timeTag
 	docker push $timeTag
 }
-docker tag skidbot-revolt:latest $targetTag
-docker push $targetTag
