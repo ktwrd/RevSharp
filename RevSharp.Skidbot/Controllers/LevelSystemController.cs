5ef3297506532943f3a52452f10cde55ff07998b
kate <kate@dariox.club>
2023-06-22 07:49:13 +0800

    [Untested] Xp System Prototype ported from ktwrd/skidbot

---
+++ RevSharp.Skidbot/Controllers/LevelSystemController.cs
@@ -0,0 +1,115 @@
+using MongoDB.Driver;
+using RevSharp.Core.Models;
+using RevSharp.Skidbot.Helpers;
+using RevSharp.Skidbot.Models;
+using RevSharp.Skidbot.Modules;
+
+namespace RevSharp.Skidbot.Controllers;
+
+public class LevelSystemController : BaseMongoController<LevelMemberModel>
+{
+    public LevelSystemController()
+        : base("levelSystem")
+    {
+        _random = new Random();
+    }
+
+    private readonly Random _random;
+
+    public override async Task MessageReceived(Message message)
+    {
+        if (message.IsSystemMessage || message.Masquerade != null)
+            return;
+
+        var server = await message.FetchServer();
+        if (server == null)
+            return;
+        var data = await Get(message.AuthorId, server.Id);
+        if (data == null)
+            data = new LevelMemberModel()
+            {
+                UserId = message.AuthorId,
+                ServerId = server.Id
+            };
+        await Set(data);
+        
+        var currentTimestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        var previousMessageDiff = currentTimestamp - data.LastMessageTimestamp;
+        if (previousMessageDiff >= 8000)
+        {
+            var (levelUp, metadata) = await GrantXp(data, message);
+            if (levelUp)
+            {
+                await message.Reply(new SendableEmbed()
+                {
+                    Description = $"You've advanced to *level {metadata.UserLevel}*!"
+                });
+            }
+        }
+    }
+    
+    public async Task<(bool, ExperienceMetadata)> GrantXp(LevelMemberModel model, Message message)
+    {
+        var data = await Get(model.UserId, model.ServerId);
+        if (data == null)
+            data = new LevelMemberModel()
+            {
+                ServerId = model.ServerId,
+                UserId = model.UserId
+            };
+        await Set(data);
+        var amount = (ulong)_random.Next(1, 5);
+
+        // Generate previous and current metadata
+        var metadataPrevious = XpHelper.Generate(data);
+        data.Xp += amount;
+        var metadata = XpHelper.Generate(data);
+
+        // Set previous Ids
+        data.LastMessageChannelId = message.ChannelId;
+        data.LastMessageId = message.Id;
+
+        bool levelUp = metadataPrevious.UserLevel != metadata.UserLevel;
+        if (levelUp)
+        {
+            OnUserLevelUp(model, metadataPrevious, metadata);
+        }
+
+        await Set(data);
+        return (levelUp, metadata);
+    }
+    protected void OnUserLevelUp(LevelMemberModel model, ExperienceMetadata previous, ExperienceMetadata current)
+    {
+        if (UserLevelUp != null)
+        {
+            UserLevelUp?.Invoke(model, previous, current);
+        }
+    }
+    public event ExperienceComparisonDelegate UserLevelUp;
+
+    public async Task<LevelMemberModel?> Get(string userId, string serverId)
+    {
+        var collection = GetCollection();
+        var filter = Builders<LevelMemberModel>
+            .Filter
+            .Where(v => v.ServerId == serverId && v.UserId == userId);
+
+        var result = await collection.FindAsync(filter);
+        var item = result.FirstOrDefault();
+        return item;
+    }
+
+    public async Task Set(LevelMemberModel model)
+    {
+        var collection = GetCollection();
+        var filter = Builders<LevelMemberModel>
+            .Filter
+            .Where(v => v.ServerId == model.ServerId && v.UserId == model.UserId);
+
+        var exists = (await collection.FindAsync(filter))?.Any() ?? false;
+        if (exists)
+            await collection.ReplaceOneAsync(filter, model);
+        else
+            await collection.InsertOneAsync(model);
+    }
+}
---
+++ RevSharp.Skidbot/Modules/XpModule.cs
@@ -0,0 +1,108 @@
+using RevSharp.Core.Models;
+using RevSharp.Skidbot.Controllers;
+using RevSharp.Skidbot.Helpers;
+using RevSharp.Skidbot.Models;
+using RevSharp.Skidbot.Reflection;
+
+namespace RevSharp.Skidbot.Modules;
+
+[RevSharpModule]
+public class XpModule : BaseModule
+{
+    public override async Task CommandReceived(CommandInfo info, Message message)
+    {
+        string action = "help";
+        if (info.Arguments.Count > 0)
+            action = info.Arguments[0];
+
+        switch (action)
+        {
+            case "help":
+                await Command_Help(info, message);
+                break;
+            case "profile":
+                await Command_Profile(info, message);
+                break;
+            default:
+                await Command_Help(info, message);
+                break;
+        }
+    }
+
+    public async Task Command_Profile(CommandInfo info, Message message)
+    {
+        var server = await message.FetchServer();
+        if (server == null)
+        {
+            await message.Reply(
+                new SendableEmbed()
+                {
+                    Title = "Xp System",
+                    Description = "The Experience System can only be used in servers.",
+                    Colour = "red"
+                });
+            return;
+        }
+        await message.Reply(await GetProfile(message.AuthorId, server.Id));
+    }
+
+    public async Task Command_Help(CommandInfo info, Message message)
+    {
+        await message.Reply(
+            new SendableEmbed()
+            {
+                Title = "Xp System - Help",
+                Description = HelpContent()
+            });
+    }
+
+    public async Task<SendableEmbed> GetProfile(string userId, string serverId)
+    {
+        try
+        {
+            var controller = Reflection.FetchModule<LevelSystemController>();
+            var data = await controller.Get(userId, serverId) ?? new LevelMemberModel();
+            var metadata = XpHelper.Generate(data);
+            return new SendableEmbed()
+            {
+                Title = "Xp System - Profile",
+                Description = string.Join(
+                    "\n", new string[]
+                    {
+                        $"**XP**: {data?.Xp ?? 0}",
+                        $"**Progress**: {Math.Round(metadata.NextLevelProgress * 100, 3)}% ({metadata.UserXp - metadata.CurrentLevelStart}/{metadata.CurrentLevelEnd})",
+                        $"**Level**: {metadata.UserLevel}"
+                    })
+            };
+        }
+        catch (Exception ex)
+        {
+            Log.Error($"Failed to get profile for user {userId} (server: {serverId})\n{ex}");
+            return new SendableEmbed()
+            {
+                Title = "Xp System - Profile",
+                Description = string.Join(
+                    "\n", new string[]
+                    {
+                        "Failed to fetch profile", $"`{ex.Message}`"
+                    })
+            };
+        }
+    }
+
+    public override string? HelpContent()
+    {
+        var r = Program.ConfigData.Prefix + BaseCommandName;
+        return string.Join(
+            "\n", new string[]
+            {
+                "```",
+                $"{r} help      - display this message",
+                $"{r} profile   - Get XP profile"
+            });
+    }
+    public override bool HasHelpContent => false;
+    public override string? InternalName => "other";
+    public override string? HelpCategory => "xp";
+    public override string? BaseCommandName => "xp";
+}
