ce713b7302f89b1424ad291d3152911a1cd79a54
kate <kate@dariox.club>
2023-06-22 20:26:22 +0800

    Add 1984 imgwiz

--- RevSharp.Skidbot.ImgWiz/Controllers/ImageWizardController.cs
+++ RevSharp.Skidbot.ImgWiz/Controllers/ImageWizardController.cs
@@ -1,11 +1,16 @@
 using NetVips;
+using RevSharp.Core.Models;
+using RevSharp.Skidbot.Helpers;
 using RevSharp.Skidbot.Reflection;
+using System.Diagnostics;
+using File = System.IO.File;
 using NVIPS = NetVips.NetVips;
+using RevoltFile = RevSharp.Core.Models.File;
 
 namespace RevSharp.Skidbot.ImgWiz.Controllers;
 
 [RevSharpModule]
-public class ImageWizardController : BaseModule
+public partial class ImageWizardController : BaseModule
 {
     public override async Task Initialize(ReflectionInclude reflectionInclude)
     {
@@ -18,4 +23,163 @@ public class ImageWizardController : BaseModule
             Log.Error($"Failed to init libvips\n{ModuleInitializer.Exception.Message}");
         }
     }
+
+    public override async Task CommandReceived(CommandInfo info, Message message)
+    {
+        string action = "help";
+        if (info.Arguments.Count > 0)
+        {
+            action = info.Arguments[0].ToLower();
+        }
+
+        switch (action)
+        {
+            case "help":
+                await Command_Help(info, message);
+                break;
+            case "1984":
+                await Command_1984(info, message);
+                break;
+        }
+    }
+
+    private async Task Command_Help(CommandInfo info, Message message)
+    {
+        await message.Reply(
+            new SendableEmbed()
+            {
+                Title = "Image Wizard - Help",
+                Description = HelpContent()
+            });
+    }
+    public async Task UploadPng(Message message, Image img)
+    {
+
+        var pngStream = new MemoryStream(img.PngsaveBuffer());
+        var uploadId = await Client.UploadFile(
+            pngStream,
+            "image.png",
+            "attachments",
+            "image/png");
+        if (uploadId != null)
+        {
+            await message.Reply(new DataMessageSend()
+            {
+                Attachments = new string[] { uploadId }
+            });
+        }
+        else
+        {
+            await message.Reply($"Failed to upload image");
+        }
+    }
+
+    internal string GetAfterArgs(CommandInfo info)
+    {
+        string caption = "";
+        if (info.Arguments.Count >= 2)
+        {
+            var tmpArgs = new List<string>(info.Arguments);
+            tmpArgs.RemoveAt(0);
+            if (tmpArgs.Count > 0)
+            {
+                caption = string.Join(" ", tmpArgs);
+            }
+        }
+        return caption;
+    }
+    internal string[] ParsePrompt(CommandInfo info, string defaultValue)
+    {
+        var caption = GetAfterArgs(info);
+
+        var captionSplit = caption.Split("|").ToList();
+        if (captionSplit.Count < 2)
+        {
+            captionSplit.Add(defaultValue);
+        }
+        return captionSplit.ToArray();
+    }
+    
+    private Image Normalize(Image img)
+    {
+        if (img.Bands < 3)
+            img = img.Colourspace(Enums.Interpretation.Srgb);
+
+        if (img.Bands == 3)
+            img = img.Bandjoin(255);
+
+        return img;
+    }
+    private async Task AttemptFontExtract()
+    {
+        if (!Directory.Exists(FeatureFlags.FontCache))
+            Directory.CreateDirectory(FeatureFlags.FontCache);
+
+        foreach (var pair in FontFilenamePairs)
+        {
+            var outputLocation = GetFontLocation(pair.Key);
+            if (File.Exists(outputLocation))
+                continue;
+            var obj = ImageWizard.ResourceManager.GetObject(pair.Key) as byte[];
+            using (var rs = new MemoryStream(obj))
+            using (var fs = new FileStream(outputLocation, FileMode.Create, FileAccess.Write))
+            {
+                await rs.CopyToAsync(fs);
+            }
+        }
+    }
+    public static Dictionary<string, string> FontFilenamePairs => new Dictionary<string, string>()
+    {
+        {"font_arial", "arial.ttf"},
+        {"font_AtkinsonHyperlegible_Bold", "AtkinsonHyperlegible-Bold.ttf"},
+        {"font_caption", "caption.otf"},
+        {"font_chirp_regular_web", "chip-regular-web.woff2"},
+        {"font_HelveticaNeue", "HelveticaNeue.otf"},
+        {"font_ImpactMix", "ImpactMix.ttf"},
+        {"font_TAHOMABD", "TAHOMABD.TTF"},
+        {"font_times_new_roman", "times new roman.ttf"},
+        {"font_TwemojiCOLR0", "TwemojiCOLR0.otf"},
+        {"font_Ubuntu_R", "Ubuntu-R.ttf"},
+        {"font_whisper", "whisper.otf"}
+    };
+    private string GetFontLocation(string font)
+    {
+        return Path.Combine(FeatureFlags.FontCache, FontFilenamePairs[font]);
+    }
+
+    public async Task<RevoltFile?> GetTargetFile(Message message)
+    {
+        if (message.Attachments?.Length > 0)
+        {
+            foreach (var i in message.Attachments)
+            {
+                if (i.Metadata.Type == "Image")
+                {
+                    return i;
+                }
+            }
+        }
+
+        var channel = await Client.GetChannel(message.ChannelId);
+        if (message.MessageReplyIds?.Length > 0)
+        {
+            foreach (var id in message.MessageReplyIds)
+            {
+                var innerMessage = await channel.GetMessage(id);
+                if (innerMessage == null)
+                    continue;
+
+                var f = await GetTargetFile(innerMessage);
+                if (f != null)
+                    return f;
+            }
+        }
+
+        return null;
+    }
+
+    public override bool HasHelpContent => true;
+    public override string? InternalName => "imgwiz";
+    public override string? HelpCategory => "fun";
+    public override string? BaseCommandName => "imgwiz";
 }
---
+++ RevSharp.Skidbot.ImgWiz/Controllers/ImgWiz/1984.cs
@@ -0,0 +1,58 @@
+﻿using NetVips;
+using RevSharp.Core.Models;
+using RevSharp.Skidbot.Helpers;
+using RevSharp.Skidbot.Reflection;
+using System.Diagnostics;
+using File = System.IO.File;
+using NVIPS = NetVips.NetVips;
+using RevoltFile = RevSharp.Core.Models.File;
+
+namespace RevSharp.Skidbot.ImgWiz.Controllers
+{
+    public partial class ImageWizardController
+    {
+        private async Task Command_1984(CommandInfo info, Message message)
+        {
+            await AttemptFontExtract();
+            var embed = new SendableEmbed()
+            {
+                Title = "Image Wizard = 1984"
+            };
+            var captionSplit = ParsePrompt(info, "JANUARY 1984");
+
+            bool originaldate = captionSplit[1].ToLower() != "january 1984";
+
+            var img = Image.NewFromBuffer(ImageWizard.img_1984originaldate);
+            img = Image.NewFromBuffer(ImageWizard.img_1984);
+            var speechBubble = Image.Text(
+                captionSplit[0],
+                font: "Atkinson Hyperlegible Bold",
+                rgba: true,
+                fontfile: GetFontLocation("font_AtkinsonHyperlegible_Bold"),
+                align: Enums.Align.Centre,
+                width: 290,
+                height: 90);
+            speechBubble.Gravity(Enums.CompassDirection.Centre, 290, 90, extend: Enums.Extend.Black);
+            img = img.Composite2(speechBubble, Enums.BlendMode.Over, x: 60, y: 20);
+
+            if (!originaldate)
+            {
+                var dateText = Image.Text(
+                    captionSplit[1].ToUpper(),
+                    font: "ImpactMix",
+                    rgba: true,
+                    fontfile: GetFontLocation("font_ImpactMix"),
+                    align: Enums.Align.Centre,
+                    width: 124,
+                    height: 34);
+                dateText = dateText.Gravity(Enums.CompassDirection.Centre, 124, 34, extend: Enums.Extend.Black);
+                dateText = dateText.Affine(new double[] { 1, 0, 0.176327, 1 });
+                img = img.Composite2(dateText, Enums.BlendMode.Over, x: 454, y: 138);
+                img = img.Composite2(Normalize(Image.NewFromBuffer(ImageWizard.img_1984cover)), Enums.BlendMode.Over);
+            }
+
+            await UploadPng(message, img);
+        }
+
+    }
+}
