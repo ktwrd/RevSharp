3999e425248aa8a3b4172faac0b5aeaf55ac4b7d
kate <kate@dariox.club>
2023-06-24 14:22:32 +0800

    Add methods for adding more of things to a message

--- RevSharp.Core/Models/Message/DataMessageSend.cs
+++ RevSharp.Core/Models/Message/DataMessageSend.cs
@@ -18,4 +18,51 @@ public class DataMessageSend
     public Masquerade? Masquerade { get; set; }
     [JsonPropertyName("interactions")]
     public Interactions[]? Interactions { get; set; }
+
+    /// <summary>
+    /// Add an embed to this message
+    /// </summary>
+    /// <param name="embed">Embed to add</param>
+    /// <returns>Instance of this</returns>
+    public DataMessageSend AddEmbed(SendableEmbed embed)
+    {
+        Embeds = Embeds?.Concat(new SendableEmbed[] { embed }).ToArray();
+        return this;
+    }
+    /// <summary>
+    /// Add an attachment id to this message
+    /// </summary>
+    /// <param name="id">Attachment Id to add</param>
+    /// <returns>Instance of this</returns>
+    public DataMessageSend AddAttachment(string id)
+    {
+        Attachments = Attachments?.Concat(new string[] { id }).ToArray();
+        return this;
+    }
+    /// <summary>
+    /// Add a reply to this message
+    /// </summary>
+    /// <param name="id">Message Id</param>
+    /// <param name="mention">Mention message</param>
+    /// <returns>Instance of this</returns>
+    public DataMessageSend AddReply(string id, bool mention = true)
+    {
+        Replies = Replies?.Concat(new Reply[]
+        {
+            new Reply()
+            {
+                Id = id,
+                Mention = mention
+            }
+        }).ToArray();
+        return this;
+    }
+    /// <summary>
+    /// Add a reply to this message
+    /// </summary>
+    /// <param name="message">Message that we want to reply to</param>
+    /// <param name="mention">Mention message</param>
+    /// <returns>Instance of this</returns>
+    public DataMessageSend AddReply(Message message, bool mention = true)
+        => AddReply(message.Id, mention);
 }
