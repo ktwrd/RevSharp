1b1c29a8179c1fec647334efa2055c78aa2bf053
kate <kate@dariox.club>
2023-06-27 21:19:01 +0800

    starboard module prototype

--- RevSharp.Xenia/Models/StarboardConfigModel.cs
+++ RevSharp.Xenia/Models/StarboardConfigModel.cs
@@ -5,11 +5,18 @@ public class StarboardConfigModel : BaseMongoModel
     public string ServerId { get; set; }
     public int MinimumRequired { get; set; }
     public string ChannelId { get; set; }
-
+    /// <summary>
+    /// Key: Original Message Id
+    /// Value: Message Id in ChannelId
+    /// </summary>
+    public Dictionary<string, string> ProxyMessageMap { get; set; }
+    public Dictionary<string, int> MessageReact { get; set; }
     public StarboardConfigModel()
     {
         ServerId = "";
         MinimumRequired = 3;
         ChannelId = "";
+        ProxyMessageMap = new Dictionary<string, string>();
+        MessageReact = new Dictionary<string, int>();
     }
 }
--- RevSharp.Xenia/Modules/StarboardModule.cs
+++ RevSharp.Xenia/Modules/StarboardModule.cs
@@ -8,6 +8,62 @@ namespace RevSharp.Xenia.Modules;
 
 public class StarboardModule : CommandModule
 {
+
+    public override Task InitComplete()
+    {
+        Client.MessageReactAdd += async (id, react, messageId, channelId) =>
+        {
+            if (react != "⭐")
+                return;
+            if (!(await Client.GetChannel(channelId) is TextChannel channel))
+                return;
+
+            var server = await Client.GetServer(channel.ServerId);
+            var controller = Reflection.FetchModule<StarboardConfigController>();
+            var data = await controller.Get(server.Id)
+                       ?? new StarboardConfigModel()
+                       {
+                           ServerId = server.Id
+                       };
+            await controller.Set(data);
+            if (data.ChannelId.Length < 1)
+                return;
+            if (data.ChannelId == channelId)
+                return;
+            if (data.ProxyMessageMap.ContainsKey(messageId))
+                return;
+
+            var message = await channel.GetMessage(messageId);
+
+            data = await controller.Get(server.Id);
+            data.MessageReact.TryAdd(messageId, 0);
+            data.MessageReact[messageId]++;
+            await controller.Set(data);
+            if (data.MessageReact[messageId] >= data.MinimumRequired)
+            {
+                await channel.SendMessage(
+                    new DataMessageSend()
+                    {
+                        Embeds = new SendableEmbed[]
+                        {
+                            new SendableEmbed()
+                            {
+                                Description = string.Join(
+                                    "\n",
+                                    new string[]
+                                    {
+                                        $"Author <@{message.AuthorId}>",
+                                        message.Content ?? "<empty>",
+                                    }),
+                                Media = message?.Attachments?.Select(v => v.Id).FirstOrDefault(),
+                                Url = $"https://app.revolt.chat/servers/{server.Id}/channels/{channel.Id}/{messageId}"
+                            }
+                        }
+                    });
+            }
+        };
+        return Task.CompletedTask;
+    }
     public override async Task CommandReceived(CommandInfo info, Message message)
     {
         var action = "";
@@ -75,7 +131,11 @@ public class StarboardModule : CommandModule
 
     public override string? HelpContent()
     {
-        return XeniaHelper.GenerateHelp(this, new List<(string, string)>());
+        return XeniaHelper.GenerateHelp(this, new List<(string, string)>()
+        {
+            ("setchannel", "Set channel to proxy starboard messages to"),
+            ("help", "Display this message")
+        });
     }
     public override string? HelpCategory => null;
     public override bool HasHelpContent => true;
