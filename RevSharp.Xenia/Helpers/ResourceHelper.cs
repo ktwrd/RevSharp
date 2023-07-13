f8b8a6262d92eae70a915e5652fb6674de8f0f61
kate <kate@dariox.club>
2023-07-13 21:21:21 +0800

    Add ResourceHelper

---
+++ RevSharp.Xenia/Helpers/ResourceHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace RevSharp.Xenia.Helpers;
+
+public static class ResourceHelper
+{
+    public static string GetAsString(Assembly assembly, string name)
+    {
+        using (var asmStream = assembly.GetManifestResourceStream(name))
+        using (var reader = new StreamReader(asmStream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+}
